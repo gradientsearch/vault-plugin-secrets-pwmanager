f390c45dc0c1648cb50c2d644c5294cc5006e309
Stephen O'Dwyer <67125983+gradientsearch@users.noreply.github.com>
2024-12-20 20:05:49 -0500

    add register path - a copy from config

---
+++ plugin/path_register_test.go
@@ -0,0 +1,151 @@
+package secretsengine
+
+import (
+	"context"
+	"strconv"
+	"testing"
+
+	"github.com/hashicorp/vault/sdk/logical"
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	registerName = "testpwmgr"
+	registerID   = "75e46130-bf36-11ef-8781-abab9f01266a"
+)
+
+// TestUserRegister uses a mock backend to check
+// register create, read, update, and delete.
+func TestUserRegister(t *testing.T) {
+	b, s := getTestBackend(t)
+
+	t.Run("List All Registers", func(t *testing.T) {
+		for i := 1; i <= 10; i++ {
+			_, err := testTokenRegisterCreate(t, b, s,
+				registerName+strconv.Itoa(i),
+				map[string]interface{}{
+					"username": registerID,
+					"ttl":      testTTL,
+					"max_ttl":  testMaxTTL,
+				})
+			require.NoError(t, err)
+		}
+
+		resp, err := testTokenRegisterList(t, b, s)
+		require.NoError(t, err)
+		require.Len(t, resp.Data["keys"].([]string), 10)
+	})
+
+	t.Run("Create User Register - pass", func(t *testing.T) {
+		resp, err := testTokenRegisterCreate(t, b, s, registerName, map[string]interface{}{
+			"username": registerID,
+			"ttl":      testTTL,
+			"max_ttl":  testMaxTTL,
+		})
+
+		require.Nil(t, err)
+		require.Nil(t, resp.Error())
+		require.Nil(t, resp)
+	})
+
+	t.Run("Read User Register", func(t *testing.T) {
+		resp, err := testTokenRegisterRead(t, b, s)
+
+		require.Nil(t, err)
+		require.Nil(t, resp.Error())
+		require.NotNil(t, resp)
+		require.Equal(t, resp.Data["username"], registerID)
+	})
+	t.Run("Update User Register", func(t *testing.T) {
+		resp, err := testTokenRegisterUpdate(t, b, s, map[string]interface{}{
+			"ttl":     "1m",
+			"max_ttl": "5h",
+		})
+
+		require.Nil(t, err)
+		require.Nil(t, resp.Error())
+		require.Nil(t, resp)
+	})
+
+	t.Run("Re-read User Register", func(t *testing.T) {
+		resp, err := testTokenRegisterRead(t, b, s)
+
+		require.Nil(t, err)
+		require.Nil(t, resp.Error())
+		require.NotNil(t, resp)
+		require.Equal(t, resp.Data["username"], registerID)
+	})
+
+	t.Run("Delete User Register", func(t *testing.T) {
+		_, err := testTokenRegisterDelete(t, b, s)
+
+		require.NoError(t, err)
+	})
+}
+
+// Utility function to create a register while, returning any response (including errors)
+func testTokenRegisterCreate(t *testing.T, b *pwmgrBackend, s logical.Storage, name string, d map[string]interface{}) (*logical.Response, error) {
+	t.Helper()
+	resp, err := b.HandleRequest(context.Background(), &logical.Request{
+		Operation: logical.CreateOperation,
+		Path:      "register/" + name,
+		Data:      d,
+		Storage:   s,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+// Utility function to update a register while, returning any response (including errors)
+func testTokenRegisterUpdate(t *testing.T, b *pwmgrBackend, s logical.Storage, d map[string]interface{}) (*logical.Response, error) {
+	t.Helper()
+	resp, err := b.HandleRequest(context.Background(), &logical.Request{
+		Operation: logical.UpdateOperation,
+		Path:      "register/" + registerName,
+		Data:      d,
+		Storage:   s,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if resp != nil && resp.IsError() {
+		t.Fatal(resp.Error())
+	}
+	return resp, nil
+}
+
+// Utility function to read a register and return any errors
+func testTokenRegisterRead(t *testing.T, b *pwmgrBackend, s logical.Storage) (*logical.Response, error) {
+	t.Helper()
+	return b.HandleRequest(context.Background(), &logical.Request{
+		Operation: logical.ReadOperation,
+		Path:      "register/" + registerName,
+		Storage:   s,
+	})
+}
+
+// Utility function to list registers and return any errors
+func testTokenRegisterList(t *testing.T, b *pwmgrBackend, s logical.Storage) (*logical.Response, error) {
+	t.Helper()
+	return b.HandleRequest(context.Background(), &logical.Request{
+		Operation: logical.ListOperation,
+		Path:      "register/",
+		Storage:   s,
+	})
+}
+
+// Utility function to delete a register and return any errors
+func testTokenRegisterDelete(t *testing.T, b *pwmgrBackend, s logical.Storage) (*logical.Response, error) {
+	t.Helper()
+	return b.HandleRequest(context.Background(), &logical.Request{
+		Operation: logical.DeleteOperation,
+		Path:      "register/" + registerName,
+		Storage:   s,
+	})
+}

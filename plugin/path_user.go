5eab7e90417c3cac79a8efcc43af0a39a78cf019
Stephen O'Dwyer <67125983+gradientsearch@users.noreply.github.com>
2024-12-24 08:46:20 -0500

    move register path to user path

--- plugin/path_user.go
+++ plugin/path_user.go
@@ -5,9 +5,12 @@ import (
 	"fmt"
 	"time"
 
+	mapstructure "github.com/go-viper/mapstructure/v2"
 	"github.com/hashicorp/vault/sdk/framework"
 	"github.com/hashicorp/vault/sdk/logical"
 )
+
+const USER_SCHEMA string = "users"
 
 // pwmgrUserEntry defines the data required
 // for a Vault user to access and call the Pwmgr
@@ -15,10 +18,28 @@ import (
 type pwmgrUserEntry struct {
 	Username string        `json:"username"`
 	UserID   int           `json:"user_id"`
+	EntityID string        `json:"entity_id"`
 	Token    string        `json:"token"`
 	TokenID  string        `json:"token_id"`
 	TTL      time.Duration `json:"ttl"`
 	MaxTTL   time.Duration `json:"max_ttl"`
+	UUK      pwmgrUUKEntry `json:"uuk"`
+}
+
+// pwmgrUUKEntry defines the data required
+// for a Vault register to access and call the Pwmgr
+// token endpoints
+type pwmgrUUKEntry struct {
+	// uuid of priv key
+	UUID string `json:"uuid"`
+	// symmetric key used to encrypt the EncPriKey
+	EncSymKey EncSymKey `json:"enc_sym_key"`
+	// mp a.k.a secret key
+	EncryptedBy string `json:"encrypted_by"`
+	// priv key used to encrypt `Safe` data
+	EncPriKey EncPriKey `json:"enc_pri_key"`
+	// pub key of the private key
+	PubKey map[string]string `json:"pubkey"`
 }
 
 // toResponseData returns response data for a user
@@ -38,6 +59,44 @@ func (r *pwmgrUserEntry) toResponseData() map[string]interface{} {
 // path patterns to list all users.
 func pathUser(b *pwmgrBackend) []*framework.Path {
 	return []*framework.Path{
+		{
+			Pattern: "register",
+			Fields: map[string]*framework.FieldSchema{
+				"uuid": {
+					Type:        framework.TypeLowerCaseString,
+					Description: "unique id",
+					Required:    true,
+				},
+				"enc_sym_key": {
+					Type:        framework.TypeMap,
+					Description: "encrypted key that encrypts the private key",
+					Required:    true,
+				},
+				"encrypted_by": {
+					Type:        framework.TypeString,
+					Description: "UUID of the key that encrypts the encSymmetricKey",
+					Required:    true,
+				},
+				"pubKey": {
+					Type:        framework.TypeMap,
+					Description: "public part of the key pair",
+					Required:    true,
+				},
+				"enc_pri_key": {
+					Type:        framework.TypeMap,
+					Description: "private part of the key pair",
+					Required:    true,
+				},
+			},
+			ExistenceCheck: b.pathUserExistenceCheck,
+			Operations: map[logical.Operation]framework.OperationHandler{
+				logical.CreateOperation: &framework.PathOperation{
+					Callback: b.pathRegistersWrite,
+				},
+			},
+			HelpSynopsis:    pathRegisterHelpSynopsis,
+			HelpDescription: pathRegisterHelpDescription,
+		},
 		{
 			Pattern: "user/" + framework.GenericNameRegex("name"),
 			Fields: map[string]*framework.FieldSchema{
@@ -173,15 +232,88 @@ func (b *pwmgrBackend) pathUsersDelete(ctx context.Context, req *logical.Request
 	return nil, nil
 }
 
-// setUser adds the user to the Vault storage API
-func setUser(ctx context.Context, s logical.Storage, name string, userEntry *pwmgrUserEntry) error {
-	entry, err := logical.StorageEntryJSON("user/"+name, userEntry)
+// pathRegistersWrite makes a request to Vault storage to register a users UUK.
+func (b *pwmgrBackend) pathRegistersWrite(ctx context.Context, req *logical.Request, d *framework.FieldData) (*logical.Response, error) {
+	registerUser, err := b.getUser(ctx, req.Storage, req.EntityID)
+	if err != nil {
+		return nil, err
+	}
+
+	if registerUser != nil {
+		return logical.ErrorResponse("user already registered"), nil
+	}
+
+	registerEntry := pwmgrUUKEntry{}
+
+	createOperation := (req.Operation == logical.CreateOperation)
+
+	if uuid, ok := d.GetOk("uuid"); ok {
+		registerEntry.UUID = uuid.(string)
+	} else if !ok && createOperation {
+		return nil, fmt.Errorf("missing username in register")
+	}
+
+	if encSymKey, ok := d.GetOk("enc_sym_key"); ok {
+		if err := mapstructure.Decode(encSymKey, &registerEntry.EncSymKey); err != nil {
+			return logical.ErrorResponse("error decoding encSymKey"), nil
+		}
+	} else if createOperation {
+		return logical.ErrorResponse("must have encSymKey"), nil
+	}
+
+	if encryptedBy, ok := d.GetOk("encrypted_by"); ok {
+		registerEntry.EncryptedBy = encryptedBy.(string)
+	} else if createOperation {
+		return logical.ErrorResponse("must have encryptedBy"), nil
+	}
+
+	if encPriKey, ok := d.GetOk("enc_pri_key"); ok {
+		if err := mapstructure.Decode(encPriKey, &registerEntry.EncPriKey); err != nil {
+			return logical.ErrorResponse("error decoding encPriKey"), nil
+		}
+	} else if createOperation {
+		return logical.ErrorResponse("must have encPriKey"), nil
+	}
+
+	if pubkey, ok := d.GetOk("pubKey"); ok {
+		if err := mapstructure.Decode(pubkey, &registerEntry.PubKey); err != nil {
+			return logical.ErrorResponse("error decoding encPriKey"), nil
+		}
+
+	} else if createOperation {
+		return logical.ErrorResponse("must have pubkey"), nil
+	}
+
+	userEntry := pwmgrUserEntry{}
+	userEntry.EntityID = req.EntityID
+	userEntry.UUK = registerEntry
+
+	if err := setUser(ctx, req.Storage, req.EntityID, &userEntry); err != nil {
+		return nil, err
+	}
+
+	return nil, nil
+}
+
+// pathConfigExistenceCheck verifies if the configuration exists.
+func (b *pwmgrBackend) pathUserExistenceCheck(ctx context.Context, req *logical.Request, data *framework.FieldData) (bool, error) {
+	out, err := req.Storage.Get(ctx, fmt.Sprintf("%s/%s", USER_SCHEMA, req.EntityID))
+	if err != nil {
+		return false, fmt.Errorf("existence check failed: %w", err)
+	}
+
+	return out != nil, nil
+}
+
+// setRegister adds the register to the Vault storage API
+func setUser(ctx context.Context, s logical.Storage, entityID string, registerEntry *pwmgrUserEntry) error {
+	entry, err := logical.StorageEntryJSON(fmt.Sprintf("%s/%s", USER_SCHEMA, entityID), registerEntry)
 	if err != nil {
 		return err
 	}
 
 	if entry == nil {
-		return fmt.Errorf("failed to create storage entry for user")
+		return fmt.Errorf("failed to create storage entry for register")
 	}
 
 	if err := s.Put(ctx, entry); err != nil {
@@ -191,13 +323,13 @@ func setUser(ctx context.Context, s logical.Storage, name string, userEntry *pwm
 	return nil
 }
 
-// getUser gets the user from the Vault storage API
-func (b *pwmgrBackend) getUser(ctx context.Context, s logical.Storage, name string) (*pwmgrUserEntry, error) {
-	if name == "" {
-		return nil, fmt.Errorf("missing user name")
+// getUser gets the register from the Vault storage API
+func (b *pwmgrBackend) getUser(ctx context.Context, s logical.Storage, entityID string) (*pwmgrUserEntry, error) {
+	if entityID == "" {
+		return nil, fmt.Errorf("missing register entity ID")
 	}
 
-	entry, err := s.Get(ctx, "user/"+name)
+	entry, err := s.Get(ctx, fmt.Sprintf("%s/%s", USER_SCHEMA, entityID))
 	if err != nil {
 		return nil, err
 	}
@@ -206,12 +338,12 @@ func (b *pwmgrBackend) getUser(ctx context.Context, s logical.Storage, name stri
 		return nil, nil
 	}
 
-	var user pwmgrUserEntry
+	var register pwmgrUserEntry
 
-	if err := entry.DecodeJSON(&user); err != nil {
+	if err := entry.DecodeJSON(&register); err != nil {
 		return nil, err
 	}
-	return &user, nil
+	return &register, nil
 }
 
 const (
@@ -223,4 +355,10 @@ You can configure a user to manage a user's token by setting the username field.
 
 	pathUserListHelpSynopsis    = `List the existing users in Pwmgr backend`
 	pathUserListHelpDescription = `Users will be listed by the user name.`
+
+	pathRegisterHelpSynopsis    = `Manages the Vault register endpoint for users to store their UUK.`
+	pathRegisterHelpDescription = `
+This path allows you a user to register with the pwmanager. Upon successful 
+registration, the user (i.e. entityID and UUK) is added to the users schema.
+`
 )

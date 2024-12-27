d7741162f1fa1bf0e48f5ab99977ba55d7cc1cfd
Stephen O'Dwyer <67125983+gradientsearch@users.noreply.github.com>
2024-12-27 17:37:07 -0500

    add sidebar to unlocked components

--- client/src/layouts/unlocked.svelte
+++ client/src/layouts/unlocked.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
 	import Header from "../components/unlocked/header.svelte";
 	import Password from "../components/unlocked/password.svelte";
+	import Sidebar from "../components/unlocked/sidebar.svelte";
 	import Vault from "../components/unlocked/vault.svelte";
 
 	let bg = ['bg-sky-500', 'bg-slate-500', 'bg-pink-400', 'bg-green-400'];
 </script>
 
 <div class="flex h-full">
-	<div class="{bg[0]} w-full md:bg-green-500 md:max-w-64 lg:block hidden  content-center text-center">(sidebar)</div>
+	<Sidebar></Sidebar>
 	<div class="h-full w-full flex-col">
 		<Header></Header>
 		<div class="flex h-[calc(100vh-48px)] w-full">

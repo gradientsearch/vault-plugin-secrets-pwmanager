940a0acd56cf8581d1dad682683a7f54aff06079
Stephen O'Dwyer <67125983+gradientsearch@users.noreply.github.com>
2024-12-27 23:48:56 -0500

    add vault colors. add enroll and register cards to home screen

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -5,5 +5,50 @@
 	//goto(`${base}/locked`)
 </script>
 
-<div class="bg-primary-500 h-full w-fill"></div>
-<div class="bg-secondary-600 h-full w-fill"></div>
+<div class="flex h-full w-full items-center justify-center">
+	<div class="flex-row">
+		<div class="border-border_faint relative block overflow-hidden border p-3 shadow-lg">
+			<div class="sm:flex sm:justify-between sm:gap-4">
+				<div>
+					<h3 class="text-gray-900 text-lg font-bold sm:text-xl">Enroll New Device</h3>
+				</div>
+			</div>
+
+			<div class="mt-4">
+				<p class="text-gray-500 text-pretty text-sm">
+					Choose this option if you wish to enroll a new device with this password manager.
+				</p>
+			</div>
+
+			<!-- Base -->
+
+			<a
+				class="bg-blue-200 text-foreground_high_contrast hover:bg-blue-400 mt-6 inline-block w-[76px] rounded border py-2 text-center text-sm font-medium"
+				href="{base}/enroll"
+			>
+				Enroll
+			</a>
+		</div>
+
+		<div class="border-border_faint relative mt-14 block overflow-hidden border p-3 shadow-lg">
+			<div class="sm:flex sm:justify-between sm:gap-4">
+				<div>
+					<h3 class="text-gray-900 text-lg font-bold sm:text-xl">Register</h3>
+				</div>
+			</div>
+
+			<div class="mt-4">
+				<p class="text-gray-500 text-pretty text-sm">
+					Choose this option to register as a first-time user with this password manager.
+				</p>
+			</div>
+
+			<a
+				class="bg-blue-200 text-foreground_high_contrast hover:bg-blue-400 mt-6 inline-block w-[76px] rounded border py-2 text-center text-sm font-medium"
+				href="{base}/register"
+			>
+				Register
+			</a>
+		</div>
+	</div>
+</div>

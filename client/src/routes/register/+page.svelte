80b375878a7864a5abcf9807a09a0da92613465b
Stephen O'Dwyer <67125983+gradientsearch@users.noreply.github.com>
2024-12-29 23:24:24 -0500

    add back register page and form component

---
+++ client/src/routes/register/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Title from '../../components/title.svelte';
+	import VaultIconAndText from '../../components/vaultIconAndText.svelte';
+	import Form from './components/form.svelte';
+</script>
+
+<div class="flex h-full w-full justify-center">
+	<div class="flex-row">
+		<VaultIconAndText className="mt-14"></VaultIconAndText>
+		<Title className="mb-2 mt-8">Register</Title>
+		<Form></Form>
+	</div>
+</div>

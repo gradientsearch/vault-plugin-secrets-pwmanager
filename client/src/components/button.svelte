50af89d0b65803f837aa6b49663d317ffe2d145e
Stephen O'Dwyer <67125983+gradientsearch@users.noreply.github.com>
2024-12-30 23:49:34 -0500

    add progress spinner on register

--- client/src/components/button.svelte
+++ client/src/components/button.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { base } from '$app/paths';
 
-	var { href = '', fn = null, children } = $props();
+	var { href = '', fn = null, disabled = false, children } = $props();
 </script>
 
 {#if href.length > 0}
@@ -15,6 +15,7 @@
 	<button
 		class="mt-6 inline-block w-[76px] rounded border bg-blue-200 py-2 text-center text-sm font-bold text-foreground_high_contrast hover:bg-blue-400"
 		onclick={fn}
+		disabled={disabled}
 	>
 		{@render children?.()}
 	</button>
--- client/src/routes/register/components/form.svelte
+++ client/src/routes/register/components/form.svelte
@@ -15,6 +15,8 @@
 		secretKey: Uint8Array = new Uint8Array();
 	}
 
+	let errorText: string | undefined = undefined;
+
 	let register = new Register();
 	let encoder = new TextEncoder();
 	let isRegistering = false;
@@ -22,22 +24,23 @@
 		isRegistering = true;
 		let randomSeq = bytesToHex(crypto.getRandomValues(new Uint8Array(26)));
 		let secretKey = `H1-${register.mount}-${randomSeq}`; // combination Secret ID - secret
-		// TODO grab entity-id from token
 
-        let api = new Api(register.token, register.url, register.mount)
-        let tokenInfo = await api.tokenLookup()
-        let entityID = tokenInfo['data']['entity_id']
+		let api = new Api(register.token, register.url, register.mount);
+		let tokenInfo = await api.tokenLookup();
+		let entityID = tokenInfo['data']['entity_id'];
 		let uuk = await buildUUK(
 			encoder.encode(register.password),
 			encoder.encode(register.mount),
 			encoder.encode(secretKey),
-			encoder.encode(entityID),
+			encoder.encode(entityID)
 		);
 
-        let err =  await api.register(uuk)
-        if (err != undefined){
-            console.log("error registering")
-        }
+		let err = await api.register(uuk);
+		if (err != undefined) {
+			errorText = err.message
+		}
+
+		isRegistering = false;
 	}
 </script>
 
@@ -93,7 +96,38 @@
 						bind:value={register.retypedPassword}
 					/>
 				</label>
-				<Button fn={onRegister}>Register</Button>
+
+				{#if errorText}
+					<p class="text-sm text-foreground_critical">{errorText}</p>
+				{/if}
+
+				{#if !isRegistering}
+					<Button disabled={isRegistering} fn={onRegister}>Register</Button>
+				{:else}
+					<div class="flex items-center justify-center">
+						<svg
+							class="text-white h-3 w-3 animate-spin"
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+						>
+							<circle
+								class="opacity-25"
+								cx="12"
+								cy="12"
+								r="10"
+								stroke="currentColor"
+								stroke-width="4"
+							></circle>
+							<path
+								class="opacity-75"
+								fill="currentColor"
+								d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+							></path>
+						</svg>
+						<span class="ml-1 text-sm">Processing</span>
+					</div>
+				{/if}
 			</div>
 		</CardContainer>
 	</div>

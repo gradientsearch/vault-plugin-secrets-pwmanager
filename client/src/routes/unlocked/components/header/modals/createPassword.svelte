0cbfe222b4ec9e3cb0ec5182242baf5508263fe5
Stephen O'Dwyer <67125983+gradientsearch@users.noreply.github.com>
2025-01-03 16:51:03 -0500

    add note and ssh passwords

--- client/src/routes/unlocked/components/header/modals/createPassword.svelte
+++ client/src/routes/unlocked/components/header/modals/createPassword.svelte
@@ -13,4 +13,14 @@
         <img class="h-16" src='{base}/icons/key.svg' alt="key icon">
         <span class="md:absolute  left-0 right-0  flex-1 text-center">Login</span>
     </button>
+
+    <button class="relative min-h-32  bg-page_faint shadow-lg flex md:flex-row flex-col justify-items-start items-center hover:bg-surface_interactive_hover">
+        <img class="h-16" src='{base}/icons/note.png' alt="note icon">
+        <span class="md:absolute  left-0 right-0  flex-1 text-center">Note</span>
+    </button>
+
+    <button class="relative min-h-32  bg-page_faint shadow-lg flex md:flex-row flex-col justify-items-start items-center hover:bg-surface_interactive_hover">
+        <img class="h-16" src='{base}/icons/ssh.png' alt="ssh icon">
+        <span class="md:absolute  left-0 right-0  flex-1 text-center">ssh</span>
+    </button>
 </div>

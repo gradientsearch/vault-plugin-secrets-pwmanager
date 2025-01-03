349a80a86972adad929d6dccf668de725820611d
Stephen O'Dwyer <67125983+gradientsearch@users.noreply.github.com>
2025-01-03 16:55:53 -0500

    add hover:text-color on password types

--- client/src/routes/unlocked/components/header/modals/createPassword.svelte
+++ client/src/routes/unlocked/components/header/modals/createPassword.svelte
@@ -9,18 +9,18 @@
 </header>
 
 <div class="grid grid-cols-3 gap-4 p-4">
-    <button class="relative min-h-32  bg-page_faint shadow-lg flex md:flex-row flex-col justify-items-start items-center hover:bg-surface_interactive_hover">
+    <button class="relative min-h-32  bg-page_faint shadow-lg flex md:flex-row flex-col justify-items-start items-center hover:bg-surface_interactive_hover hover:text-foreground_action_hover">
         <img class="h-16" src='{base}/icons/key.svg' alt="key icon">
         <span class="md:absolute  left-0 right-0  flex-1 text-center">Login</span>
     </button>
 
-    <button class="relative min-h-32  bg-page_faint shadow-lg flex md:flex-row flex-col justify-items-start items-center hover:bg-surface_interactive_hover">
+    <button class="relative min-h-32  bg-page_faint shadow-lg flex md:flex-row flex-col justify-items-start items-center hover:bg-surface_interactive_hover hover:text-foreground_action_hover">
         <img class="h-16" src='{base}/icons/note.png' alt="note icon">
         <span class="md:absolute  left-0 right-0  flex-1 text-center">Note</span>
     </button>
 
-    <button class="relative min-h-32  bg-page_faint shadow-lg flex md:flex-row flex-col justify-items-start items-center hover:bg-surface_interactive_hover">
+    <button class="relative min-h-32  bg-page_faint shadow-lg flex md:flex-row flex-col justify-items-start items-center hover:bg-surface_interactive_hover hover:text-foreground_action_hover">
         <img class="h-16" src='{base}/icons/ssh.png' alt="ssh icon">
         <span class="md:absolute  left-0 right-0  flex-1 text-center">ssh</span>
     </button>
-</div>
+</div>  

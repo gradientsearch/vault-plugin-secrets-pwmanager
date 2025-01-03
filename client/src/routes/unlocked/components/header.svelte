5e70f355d2a48790bac2224e77933ea4fe3c930b
Stephen O'Dwyer <67125983+gradientsearch@users.noreply.github.com>
2025-01-02 23:52:00 -0500

    add create password button to header

--- client/src/routes/unlocked/components/header.svelte
+++ client/src/routes/unlocked/components/header.svelte
@@ -1,1 +1,9 @@
-<header class="bg-white sticky top-0 h-[48px] w-full">(header)</header>
+<header class="sticky top-0 p-1 h-[48px]  bg-surface_faint">
+	<div class="flex justify-end">
+		<button>
+			<span class="m-4 p-2 text-sm hover:bg-surface_interactive_hover hover:text-blue-300">
+				Create password +
+			</span>
+		</button>
+	</div>
+</header>
